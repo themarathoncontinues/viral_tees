8478571c19e427928e42e06f95cd356cc9c60b59
Mitchell Bregman <mitchbregs@gmail.com>
2019-06-22 20:52:56 -0400

    minor touchups
    ; adhering to luigi framework parameter passing

--- run_luigi.py
+++ run_luigi.py
@@ -11,11 +11,10 @@ from constants import TRENDS_DIR, TRIMMED_DIR
 class QueryTwitterTrend(luigi.Task):
 
     date = luigi.DateMinuteParameter(default=datetime.now())
-    country_code = luigi.Parameter()
+    location = luigi.Parameter()
 
-    def output(self, **kwargs):
-        kwargs.setdefault('loc', self.country_code)
-        fname = 'trends_{}_{}.csv'.format(self.date.strftime('%m%d_%Y_%H%M'), kwargs['loc'])
+    def output(self):
+        fname = 'trends_{}_{}.csv'.format(self.date.strftime('%m%d_%Y_%H%M'), self.location)
         fout = TRENDS_DIR / fname
 
         return luigi.LocalTarget(fout)
@@ -24,13 +23,13 @@ class QueryTwitterTrend(luigi.Task):
         from retrieve_trends import run as retrieve_trends
 
         args_dict = {
-            'location': [self.country_code]
+            'location': [self.location]
         }
 
         df_container = retrieve_trends(args_dict)
-        f = self.output(loc=self.country_code).open('w')
+        f = self.output().open('w')
 
-        df_container[self.country_code].to_csv(f, sep=',', encoding='utf-8', index=False)
+        df_container[self.location].to_csv(f, sep=',', encoding='utf-8', index=False)
         f.close()
 
 
@@ -43,21 +42,21 @@ class TrendsTaskWrapper(luigi.WrapperTask):
                 'usa-nyc',
                 'usa-lax',
                 'usa-chi',
-                # 'usa-dal',
-                # 'usa-hou',
-                # 'usa-wdc',
-                # 'usa-mia',
-                # 'usa-phi',
-                # 'usa-atl',
-                # 'usa-bos',
-                # 'usa-phx',
-                # 'usa-sfo',
-                # 'usa-det',
-                # 'usa-sea',
+                'usa-dal',
+                'usa-hou',
+                'usa-wdc',
+                'usa-mia',
+                'usa-phi',
+                'usa-atl',
+                'usa-bos',
+                'usa-phx',
+                'usa-sfo',
+                'usa-det',
+                'usa-sea',
         ]
 
         for loc in locations:
-            yield QueryTwitterTrend(country_code=loc)
+            yield QueryTwitterTrend(location=loc)
 
         self.is_complete = True
 
@@ -90,7 +89,6 @@ class TrimTrendsData(luigi.Task):
     def run(self):
         fp = Path(self.fp)
         trimmed_df = self.trim_data(fp)
-        fout = TRIMMED_DIR / '{}_{}.csv'.format('trimmed', fp.stem)
 
         f = self.output().open('w')
         trimmed_df.to_csv(f, sep=',', encoding='utf-8', index=False)
@@ -119,32 +117,6 @@ class TrimTrendsTaskWrapper(luigi.WrapperTask):
             return True
         else:
             return False
-
-# class EmailTwitterTrends(luigi.ExternalTask):
-
-#     def requires(self):
-#         return TrendsTaskWrapper()
-
-#     def output(self):
-#         date = datetime.now()
-#         str_date = date.strftime('%m%d_%Y_%H%M')
-
-#         return luigi.LocalTarget("data/trends/trends_{}.pickle".format(str_date))
-
-#     def run(self):
-#         from send_email import run as execute, send_message
-        
-#         args_dict = {
-#             {'authentication': ['token.pickle'], 'receivers': ['mitchbregs@gmail.com'], 'attachments': self.input()}
-#         }
-
-#         auth, sender, msg = execute(args_dict)
-
-#         send_message(auth, sender, msg)
-
-#         f = self.output().open('w')
-#         pickle.dump((auth, sender, msg), f)
-#         f.close()
 
 
 if __name__ == '__main__':
