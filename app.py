299ee4859dd4c8c1a53fd3e32e0fd530f7f26873
Mitchell Bregman <mitchbregs@gmail.com>
2019-07-06 13:33:57 -0400

    more flask touchups, added product deletion and js to handle loadtime

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from models.mongo import connect_db, get_collection, retrieve_all_data, find_by_id
 from utils.constants import IMAGES_DIR
 from utils.post_shopify import get_products, delete_products
 
 
 app = Flask(__name__)
-
+app.debug = True
+app.secret_key = 'secret'
+app._static_folder = str(IMAGES_DIR)
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
+
 
 @app.route('/trends-view')
 def trend_data():
@@ -25,6 +28,7 @@ def trend_data():
 		data=data
 	)
 
+
 @app.route('/shopify-view')
 def shop_data():
 	data = get_products()
@@ -37,9 +41,23 @@ def shop_data():
 		data=data
 	)
 
+
+@app.route('/shopify-delete')
+def shop_delete():
+	data = get_products()
+
+	try:
+		delete_products(data)
+	except AssertionError:
+		return redirect(url_for('shop_data'), code=302)
+
+	return redirect(url_for('shop_data'), code=302)
+
+
 @app.route('/images-view')
 def image_data():
 	data = [x for x in IMAGES_DIR.iterdir()]
+	data = {x.name: str(x) for x in data}
 
 	return render_template(
 		'images.html',
@@ -47,5 +65,6 @@ def image_data():
 		data=data
 	)
 
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
