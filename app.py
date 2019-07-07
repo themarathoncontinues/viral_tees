2d8106fc47888a704c4492619a80f3465e5ccfb9
Mitchell Bregman <mitchbregs@gmail.com>
2019-07-07 02:30:13 -0400

    touchup

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, send_from_directory, url_for
 from models.mongo import connect_db, get_collection, retrieve_all_data, find_by_id
-from utils.constants import SRC_DIR, IMAGES_DIR, LOG_DIR
+from utils.constants import SRC_DIR, IMAGES_DIR, LOG_DIR, ENV_PATH
 from utils.post_shopify import get_products, delete_products
 
 
